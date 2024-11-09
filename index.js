239fcae22a205f773f71937daa2d5004d5501251
Linus Söderquist <66675183+linussoderquist@users.noreply.github.com>
2024-11-09 20:39:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const canvas = document.getElementById("canvas");
 
 const renderer = new SPLAT.WebGLRenderer(canvas);
 const scene = new SPLAT.Scene();
-const camera = new SPLAT.Camera();
+const camera = new SPLAT.Camera(); // Default camera with position already set
+
 const controls = new SPLAT.OrbitControls(camera, canvas);
 
 async function main() {
-    const url = "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/gs_Stump.cleaned.splat";
+  const url =
+    "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/gs_Stump.cleaned.splat";
 
-    await SPLAT.Loader.LoadAsync(url, scene, null);
+  await SPLAT.Loader.LoadAsync(url, scene, null);
 
-    const handleResize = () => {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-    };
+  const handleResize = () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
 
-    const frame = () => {
-        controls.update();
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(frame);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
+  const frame = () => {
+    controls.update();
+    renderer.render(scene, camera);
 
     requestAnimationFrame(frame);
-}
+  };
 
+  handleResize();
+  window.addEventListener("resize", handleResize);
+
+  requestAnimationFrame(frame);
+}
 main();
