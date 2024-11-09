33e33fc3c6a4ebcb5f603993bcea4222c28df3a9
Linus Söderquist <66675183+linussoderquist@users.noreply.github.com>
2024-11-09 18:48:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const camera = new SPLAT.Camera();
 const controls = new SPLAT.OrbitControls(camera, canvas);
 
 async function main() {
-    // URL till din `.ply`-fil
-    const url = "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/paludarium.compressed.ply";
+    const url =  "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/paludarium.compressed.ply";
 
     // Ladda filen till scenen
     await SPLAT.Loader.LoadAsync(url, scene, null);
