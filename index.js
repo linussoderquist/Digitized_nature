b800aed6da2875fb71fd78da826e32ac55a74ef9
Linus Söderquist <66675183+linussoderquist@users.noreply.github.com>
2024-11-10 16:17:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let controls = new SPLAT.OrbitControls(camera, canvas); // Initialize controls
 // List of splat URLs
 const splatFiles = [
   "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/gs_Stump.cleaned.splat",
-  "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/Tallticka.splat",
+  "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/Tallticka_pos.splat",
   "https://raw.githubusercontent.com/linussoderquist/Digitized_nature/main/paludarium2tilt.splat"
 ];
 
